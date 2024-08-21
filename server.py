5a4162b59eab2ad9dcd7fd948bd29595cdc2fda3
steppabot <roflgregodwai@gmail.com>
2024-08-21 08:49:03 -0500

    Update server.py

--- server.py
+++ server.py
@@ -86,6 +86,6 @@ def create_checkout_session():
         return jsonify(error=str(e)), 400
 
 if __name__ == '__main__':
-    # Use the port provided by Heroku and listen on all interfaces
+    logging.basicConfig(level=logging.INFO)
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
